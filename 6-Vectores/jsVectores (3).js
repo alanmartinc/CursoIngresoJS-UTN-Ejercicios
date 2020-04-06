f7beb22fa23f2c3e100de2973ae4fa17e98e4933
zaculizate1996 <alancabot.ac@gmail.com>
2020-04-06 11:26:40 -0300

    Se agrega ejercicios vectores

---
+++ 6-Vectores/jsVectores (3).js
@@ -0,0 +1,13 @@
+function mostrar() {
+	var miArray = new Array();
+
+	for (var i = 0; i <5; i++)
+	{
+		miArray[i]=parseInt(prompt("Ingrese un numero"));
+	}
+
+	for(var i=0; i<5; i++)
+	{
+		document.write("Posicion "+i+"-->"+miArray[i]+"<br>");
+	}
+}
